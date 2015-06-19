b714833543a4e7a537237dd52e0ef9642059b5e0
sunchen115 <sunchen115work@gmail.com>
2015-06-19 11:09:10 +0800

    refactor code

--- src/components/interview/comment.jsx
+++ src/components/interview/comment.jsx
@@ -7,17 +7,20 @@ var Comment = React.createClass({
     this.props.deleteComment(this.props.index);
   },
 
-  _handleChange: function(e){
-    if (e.keyCode == 13){
-      this.props.changeComment(this.props.index, e.target.value);
-    }
+  _handleBlur: function(e){
+    this.props.changeComment(this.props.index, e.target.value);
   },
-
+  getInitialState: function() {
+    return {value: this.props.dValue};
+  },
+  _handleChange: function(e){
+    this.setState({value: e.target.value});
+  },
   render: function() {
     return (
       <div className="row collapse">
         <div className="small-11 columns">
-          <input className='edit-input' type="text" defaultValue={this.props.dValue} onKeyDown={this._handleChange}></input>
+          <input className='edit-input' type="text" value={this.state.value} onChange={this._handleChange} onBlur={this._handleBlur}></input>
         </div>
         <div className="small-1 columns">
           <span className="icon-delete-mid" onClick={this._handleDelete}></span>
--- src/components/interview/comments.jsx
+++ src/components/interview/comments.jsx
@@ -1,12 +1,14 @@
 let React = require('react'),
     m = require('mori'),
-    CommentList = require('./comment-list');
+    Comment = require('./comment');
 
 var Comments = React.createClass({
 
   _deleteComment: function(index){
     this.setState({
-      comments: m.filter(function(comment){return m.get(comment,'index')!=index},this.state.comments)
+      comments: m.filter((comment)=>m.get(comment,'index')!=index,
+                         this.state.comments
+                        )
     });
   },
 
@@ -18,15 +20,11 @@ var Comments = React.createClass({
     });
   },
 
-
   _arrayToMori: function(commentValues){
+    var baseKey=Date.now();
     return m.toClj(
-                   commentValues.map((value,index)=>{return {value: value, index: index, key: index}})
-                   )
-  },
-
-  _generateUniqueKey: function(){
-    return Date.now();
+                   commentValues.map((value,index)=>{return {value: value, index: index, key: baseKey+"_"+index}})
+                  );
   },
 
   _addComment: function(){
@@ -38,17 +36,18 @@ var Comments = React.createClass({
   },
 
   getInitialState: function() {
-    var baseKey = this._generateUniqueKey();
     return {comments: this._arrayToMori(this.props.comments)}
   },
 
   render: function() {
+    var comments = m.intoArray(this.state.comments).map((comment)=>{
+          return (<Comment key={m.get(comment,'key')} index={m.get(comment,'index')} dValue={m.get(comment,'value')} changeComment={this._changeComment} deleteComment={this._deleteComment}></Comment>);
+        });
     return (
       <div className='row collapse comments'>
         <label>{this.props.title}<span className='icon-plus small-offset-1' onClick={this._addComment}></span></label>
-        <CommentList key={this._generateUniqueKey()} comments={this.state.comments} changeComment={this._changeComment} deleteComment={this._deleteComment}></CommentList>
+        {comments}
       </div>
-
     )
   },
 });

bb3363188b22af1364de57a63019aaa539ce52b1
sunchen115 <sunchen115work@gmail.com>
2015-06-23 18:07:45 +0800

    add delete button for interview

--- src/components/interviews.jsx
+++ src/components/interviews.jsx
@@ -8,17 +8,19 @@ var Interviews = React.createClass({
     }
   },
 
-  componentDidMount: async function() {
+  _refreshData: async function(){
     var interviews;
     if(this.props.keyword){
       interviews = await store.queryInterviews(this.props.keyword)
     }else{
-      interviews = await store.interviews(); 
+      interviews = await store.interviews();
     }
-    
     this.setState({
-      interviews: interviews.entity
-    })
+            interviews: interviews.entity
+          })
+  },
+  componentDidMount: function() {
+    this._refreshData()
   },
   componentWillUpdate: async function(nextpro, _) {
     if(this.props.keyword!=nextpro.keyword){
@@ -28,12 +30,22 @@ var Interviews = React.createClass({
       })
     }
   },
-	render: function(){
+
+  _handleDelete: async function(e){
+    if (confirm('Do you confirm to delete this interview')){
+        var key = e.target.dataset.key;
+        await store.deleteInterview(key);
+    }
+    this._refreshData()
+  },
+
+  render: function(){
     let body = this.state.interviews.map(interview=>{
       return (
         <tr>
           <td><a href={"#/interview/"+interview.key}>{interview.description}</a></td>
           <td>{interview.name}</td>
+          <td><button data-key={interview.key} className='tiny' onClick={this._handleDelete}>Delete</button></td>
         </tr>
       )
     })
@@ -44,7 +56,8 @@ var Interviews = React.createClass({
             <table summary="All interviews">
 	 		        <thead>
 			          <td width="500">Description</td>
-                <td width="200">Name</td>
+                      <td width="200">Name</td>
+                      <td width="200">Operations</td>
 		          </thead>
 	  	        <tbody>
                 {body}

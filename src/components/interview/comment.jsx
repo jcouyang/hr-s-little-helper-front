8f7d11c5c7147ac8798ed1fac48fc267ef915600
sunchen115 <sunchen115work@gmail.com>
2015-06-24 12:55:32 +0800

    add vote without save vote result

--- src/components/interview/comment.jsx
+++ src/components/interview/comment.jsx
@@ -11,20 +11,37 @@ var Comment = React.createClass({
     this.props.changeComment(this.props.index, e.target.value);
   },
   getInitialState: function() {
-    return {value: this.props.dValue};
+    return {value: this.props.dValue,
+            score: 1};
   },
   _handleChange: function(e){
     this.setState({value: e.target.value});
   },
+  _handleAddScore: function(e){
+    this.setState({score: this.state.score+1});
+  },
+  _handleReduceScore: function(e){
+    this.setState({score: this.state.score-1});
+  },
   render: function() {
     return (
       <div className="row collapse">
-        <div className="small-11 columns">
+        <div className="small-10 columns">
           <input className='edit-input' type="text" value={this.state.value} onChange={this._handleChange} onBlur={this._handleBlur}></input>
+        </div>
+        <div className="small-1 columns">
+          <span className={'label '+ (this.state.score >= 0 ? 'success': 'alert')}>{this.state.score}</span>
         </div>
         <div className="small-1 columns">
           <span className="icon-delete-mid" onClick={this._handleDelete}></span>
         </div>
+        <div className="small-1 columns">
+          <span className="icon-thumbs-o-up" onClick={this._handleAddScore}></span>
+        </div>
+        <div className="small-1 columns">
+          <span className="icon-thumbs-o-down" onClick={this._handleReduceScore}></span>
+        </div>
+
       </div>
     )
   },

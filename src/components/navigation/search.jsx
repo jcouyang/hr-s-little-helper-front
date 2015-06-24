18f3c03a402a8a747dced73ab62141f113f04326
sunchen115 <sunchen115work@gmail.com>
2015-06-24 17:47:44 +0800

    make it better look

--- src/components/navigation/search.jsx
+++ src/components/navigation/search.jsx
@@ -5,16 +5,15 @@ var Search = React.createClass({
       location.hash='/search/'+value
   },
 
-  _handleKeyDown: function(e){
-    if(e.keyCode == 13){
-      this._updateRoute(e.target.value);
-    }
+  _handleChange: function(e){
+    this._updateRoute(e.target.value);
   },
+
   render: function(){
     return (
       <div className="row collapse">
         <div className="large-13 columns">
-          <input type="search" placeholder="Search Interviewee" onKeyDown={this._handleKeyDown}></input>
+          <input type="search" placeholder="Search Interviewee" onChange={this._handleChange}></input>
         </div>
       </div>
     );

82530b460a263b353d0eed8737fe0a70e86afd9c
Jichao Ouyang <oyanglulu@gmail.com>
2015-07-14 15:03:07 +0800

    fix test

--- src/components/__tests__/bulk-upload-test.js
+++ src/components/__tests__/bulk-upload-test.js
@@ -1,7 +1,7 @@
 jest.dontMock('../interviewer-bulk-upload.process');
 describe('interviewer upload', function() {
   let m = require('mori');
-  let xf = require('../interviewer-bulk-upload.js')
+  let xf = require('../interviewer-bulk-upload.process')
   it('upload csv', function() {
     let csv = [
       ["name","email","language","experience"],
@@ -9,8 +9,19 @@ describe('interviewer upload', function() {
       ["sun","chen@sun.com", "js","2"]
     ]
     let [head, ...body] = csv;
-    expect(m.toJs(m.into(m.vector(),xf(head),body))).toBe({
-      
-    })
+    expect(m.toJs(m.into(m.vector(),xf(head),body))).toEqual([
+      {
+        email: 'ouyang@jichao.com',
+        experience: '2',
+        language: 'js',
+        name: 'jichao'
+      },
+      {
+        email: 'chen@sun.com',
+        experience: '2',
+        language: 'js',
+        name: 'sun'
+      }
+    ])
   })
 })
